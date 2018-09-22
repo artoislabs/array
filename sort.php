bb757119bec6f10e45f20d8b8ef2c761eb763292
artois <artoislabs@gmail.com>
2018-09-22 17:37:56 +0200

    sort test

---
+++ sort.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+$tab = [1, 5, 6 ,8 , 3];
+
+$res = sort($tab);
+
+var_dump($tab);
+
+/**
+ * Resultats :
+ *
+ * array (size=5)
+  0 => int 1
+  1 => int 3
+  2 => int 5
+  3 => int 6
+  4 => int 8
+ */
